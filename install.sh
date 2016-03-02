f303013eea15a0528e549ce91e99728046f123ac
Victor Gan <vigan@VIGAN-3-1013.redmond.corp.microsoft.com>
2016-03-02 14:24:16 -0800

    Bash symlinks

---
+++ install.sh
@@ -0,0 +1,17 @@
+# Installation instructions for vim.
+# do not run as sudo
+
+# Create a symbolic link
+VIMFILES_DIR="$HOME/code/vimfiles"
+ln -sfv $VIMFILES_DIR $HOME/.vim
+
+# Use file in symolic link as vimrc
+echo "source $HOME/.vim/vimrc" > $HOME/.vimrc
+
+# Better terminal colours
+echo "export TERM=xterm-256color" >> $HOME/.bashrc
+
+# For making a symbolic link (ln):
+# -s, --symbolic    make symbolic links instead of hard links
+# -f, --force       remove existing destination files
+# -v, --verbose     print name of each linked file
